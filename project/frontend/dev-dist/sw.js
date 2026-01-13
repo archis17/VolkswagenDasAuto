87748752e51fc7f43ddc154535e55e335c08c1fc
Archis Kulkarni <archiskulkarni41762@gmail.com>
2026-01-13 17:14:15 +0530

    Comeback

--- project/frontend/dev-dist/sw.js
+++ project/frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m4v8tiimnjg"
+    "revision": "0.vl2qrf75lus"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
